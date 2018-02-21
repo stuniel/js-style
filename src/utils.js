ef699d9b9291f515da37b3e07013febdd1a64294
Daniel Studziński <stuniel@gmail.com>
2018-02-21 09:57:19 +0100

    Update removePrefixes + Create close and formatInclusion methods

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,50 @@
 const utils = {
+  
+  close: function(renderer, type) {
+    switch (type) {
+      case 'inclusion':
+        renderer.push('  }')
+        break
+      default:
+        renderer.push('}')
+    }
+    renderer.push('')
+  },
 
-  removePrefix: function(key, prefix) {
-    const newKey = key
-      .indexOf(prefix) !== -1 ?
-        key.slice(prefix.length) :
-        key
-    // Check if there is a 'deeper' extension
-    return newKey.indexOf(prefix) !== -1 ?
-      removePrefix(newKey, prefix) :
+  removePrefixes: function(key, prefixes) {
+    let newKey = key
+    const prefixesList = Object
+      .keys(prefixes)
+      .map(prefix => prefixes[prefix])
+
+    prefixesList.forEach(prefix => {
+      if (key.indexOf(prefix) !== -1) {
+        newKey = key.slice(prefix.length)
+      }
+    })
+
+    return prefixesList.some(prefix => (newKey.indexOf(prefix) !== -1)) ?
+      this.removePrefixes(newKey, prefixes) :
       newKey
   },
 
-  formatOutput: function(obj, prefixes) {
+  formatInclusion: function(obj, prefixes, renderer) {
+    Object
+      .keys(obj)
+      .map(key => {
+        switch(key) {
+          case 'selector':
+            return `  ${obj[key]} {`
+          default:
+            // Check for all prefixes in prefixes object
+            let newKey = utils.removePrefixes(key, prefixes)
+            return `    ${newKey}: ${obj[key]};`
+        }
+      })
+      .forEach(key => renderer.push(key))
+  },
+
+  formatOutput: function(obj, prefixes, renderer) {
     Object
       .keys(obj)
       .map(key => {
@@ -19,21 +52,12 @@ const utils = {
           case 'selector':
             return `${obj[key]} {`
           default:
-            let newKey
             // Check for all prefixes in prefixes object
-            Object
-              .keys(prefixes)
-              .forEach(prefix => {
-                // Remove prefixes from the key
-                newKey = utils.removePrefix(key, prefixes[prefix])
-              })
-            return ` ${newKey}: ${obj[key]};`
+            let newKey = utils.removePrefixes(key, prefixes)
+            return `  ${newKey}: ${obj[key]};`
         }
       })
-      .forEach(key => console.log(key))
-    // Add closing bracket and whitespace
-    console.log('}')
-    console.log('')
+      .forEach(key => renderer.push(key))
   },
 
   formatNest: function(obj, state) {
